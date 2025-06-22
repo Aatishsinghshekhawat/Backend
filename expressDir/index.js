041de4374aeafc7d683cb0492c07c028b7862f83
Aatishsinghshekhawat <aatishsinghdbs@gmail.com>
2025-06-22 11:26:52 +0530

    started express

---
+++ expressDir/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+let port = 8080;
+
+app.listen(port, ()=>
+{
+    console.log(`App listening on port ${port}`);
+});
+
+app.use((req, res) => {
+    console.log("request recevied");
+});
